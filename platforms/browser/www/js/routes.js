bd687faf8cc4a7eaf9c42b66249199592f2ab6e4
anahi <anahicamashqgmail.com>
2020-04-01 10:07:12 -0600

    primero

---
+++ platforms/browser/www/js/routes.js
@@ -0,0 +1,19 @@
+routes = [
+  {
+    path: '/',
+    componentUrl: './pages/login.html',
+  },
+  {
+    path: '/principal/',
+    componentUrl: './pages/admin/inicio.html',
+    options: {
+      transition: 'f7-parallax',
+    },
+  },
+ 
+  // Default route, match to all pages (e.g. 404 page)
+  {
+    path: '(.*)',
+    url: './pages/404.html',
+  },
+]
---
+++ www/js/databaseconexion.js
@@ -0,0 +1,14 @@
+var conexion1=  firebase.initializeApp({
+    apiKey: "AIzaSyBNR4t4LZk-hdx9Fs0kFOnHkTmDWeIY3nk",
+    authDomain: "fghj-1fb63.firebaseapp.com",
+    databaseURL: "https://fghj-1fb63.firebaseio.com",
+    projectId: "fghj-1fb63",
+    storageBucket: "fghj-1fb63.appspot.com",
+    messagingSenderId: "407918590017",
+    appId: "1:407918590017:web:a8ec507aa03bf7a8722ec0",
+    measurementId: "G-LFYS9NN1SC"
+},'database1');
+
+
+var db1=firebase.firestore(conexion1);
+
