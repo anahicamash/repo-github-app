c48ebc334e01a22b1681ad89d3477e3a71256419
anahi <anahicamashqgmail.com>
2020-04-01 19:01:07 -0600

    Menú verbos y adjetivos sin editar

--- www/js/routes.js
+++ www/js/routes.js
@@ -10,6 +10,76 @@ routes = [
       transition: 'f7-parallax',
     },
   },
+  {
+    path: '/lecciones/',
+    componentUrl: './pages/admin/lecciones.html',
+    options: {
+      transition: 'f7-parallax',
+    },
+  },
+  {
+    path: '/verbos/',
+    componentUrl: './pages/admin/verbos.html',
+    options: {
+      transition: 'f7-parallax',
+    },
+  },
+  {
+    path: '/agregarVerbos/',
+    componentUrl: './pages/admin/agregarVerbos.html',
+    options: {
+      transition: 'f7-parallax',
+    },
+  },
+   {
+    path: '/editarVerbos/:id/:espanol/:present/:past/:participle/',
+    componentUrl: './pages/admin/editarVerbos.html',
+    options: {
+      transition: 'f7-parallax',
+    },
+  },
+  {
+    path: '/adjetivos/',
+    componentUrl: './pages/admin/adjetivos.html',
+    options: {
+      transition: 'f7-parallax',
+    },
+  },
+  {
+    path: '/agregarAdjetivos/',
+    componentUrl: './pages/admin/agregarAdjetivos.html',
+    options: {
+      transition: 'f7-parallax',
+    },
+  },
+   {
+    path: '/editarAdjetivos/:id/:espanol/:present/:past/:participle/',
+    componentUrl: './pages/admin/editarAdjetivos.html',
+    options: {
+      transition: 'f7-parallax',
+    },
+  },
+  {
+    path: '/principalU/',
+    componentUrl: './pages/usuario/inicioU.html',
+    options: {
+      transition: 'f7-parallax',
+    },
+  },
+  {
+    path: '/leccionesU/',
+    componentUrl: './pages/usuario/leccionesU.html',
+    options: {
+      transition: 'f7-parallax',
+    },
+  },
+  {
+    path: '/verbosU/',
+    componentUrl: './pages/usuario/verbosU.html',
+    options: {
+      transition: 'f7-parallax',
+    },
+  },
  
   // Default route, match to all pages (e.g. 404 page)
   {
